e5ef057835c544e1e212522db3468165526c926b
Marius Butz <mbpictures13@gmail.com>
2020-11-28 22:55:52 +0100

    implement firebase on client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,25 @@ import * as React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import {MainView} from "./views/MainView";
 import {RegisterView} from "./views/RegisterView";
+import {useEffect} from "react";
+import firebase from "firebase";
 
 export default function App() {
+
+    useEffect(() => {
+        const config = {
+            apiKey: "AIzaSyDluP0Wp9nYFNse_SdUDDBX-QHGKfDWbh0",
+            authDomain: "reflect-59598.firebaseapp.com",
+            databaseURL: "https://reflect-59598.firebaseio.com/",
+            projectId: "reflect-59598",
+            storageBucket: "reflect-59598.appspot.com",
+            messagingSenderId: "124301744749",
+            appId: "1:124301744749:web:a09d2d95c7bfb7de813d64",
+            measurementId: "G-BH2DWFHXES"
+        };
+        firebase.initializeApp(config);
+    }, [])
+
     return (
         <BrowserRouter>
             <Switch>
--- client/src/views/MainView.tsx
+++ client/src/views/MainView.tsx
@@ -1,26 +1,58 @@
 import React, {useEffect} from "react";
 import style from "./../styles/mainView.scss";
+import firebase from "firebase";
 
 export function MainView() {
     const [opacity, setOpacity] = React.useState<number>(0);
+    const [playPulse, setPlayPulse] = React.useState<boolean>(false);
 
     useEffect(() => {
+        const id = localStorage.getItem("id");
+        if (id == null) return;
+        const database = firebase.database().ref("users").child(id);
+        database.get().then((snapshot) => {
+            handleOpacityChange(snapshot.val().value);
+            if (snapshot.val().isAutomatic) {
+                const allRefs = firebase.database().ref().child("users");
+                allRefs.on("value", (snapshot) => {
+                    let cumulated = 0;
+                    let amount = 0;
+                    snapshot.forEach((child) => {
+                        if (child.val().isAutomatic) return;
+                        cumulated += child.val().value;
+                        amount++;
+                    });
+                    handleOpacityChange(cumulated / amount);
+                });
+            }
+        });
+        database.on("value", (snapshot) => {
+            if (opacity != snapshot.val().value)
+                handleOpacityChange(snapshot.val().value);
+            if (!playPulse && snapshot.val().pulse)
+                handlePlayPulse();
+        });
 
-        setTimeout(() => {
-            handleOpacityChange(Math.random());
-        }, Math.floor(Math.random() * 3000) + 2000);
-
-        // TODO: firebase
-    });
+        return () => database.off("value");
+    }, []);
 
     const handleOpacityChange = (newOpacity: number) => {
         console.log("Set opacity "  + newOpacity);
         setOpacity(newOpacity);
     }
 
+    const handlePlayPulse = () => {
+        const id = localStorage.getItem("id");
+        if (id == null) return;
+        const database = firebase.database().ref("users").child(id);
+        database.update({pulse: false}).then(() => {
+            setPlayPulse(true);
+        });
+    }
+
     return (
         <div className={style.mainView}>
-            <div className={style.representationHolder}>
+            <div className={`${style.representationHolder} ${playPulse ? style.playPulse : ""}`} onAnimationEnd={() => setPlayPulse(false)}>
                 <div className={style.representationRed}></div>
                 <div className={style.representationGreen} style={{opacity: opacity}}></div>
             </div>
